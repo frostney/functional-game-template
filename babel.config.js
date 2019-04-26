26a99fbfb7df5634473a737530d955cceb8e897e
Johannes Stein <johannesstein@freeze-dev.com>
2019-04-27 00:27:54 +0100

    Fix up tooling

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: [['@babel/env', { modules: false }]],
+    plugins: ['@babel/proposal-export-namespace-from'],
+    ignore: [/node_modules/],
+  };
+};
---
+++ packages/game/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool:
+    process.env.NODE_ENV === 'production' ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Functional Game Template',
+    }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.mjs', '.js', '.json'],
+  },
+  node: {
+    constants: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              rootMode: 'upward',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 16384,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: {
+      name: 'runtime',
+    },
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 1234,
+  },
+};
